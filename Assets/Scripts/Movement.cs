41325be194a1113f5a97ec5180eaed72202d8814
Matías Bertola <matias_bertola_cabrera@hotmail.com>
2026-09-11 16:05:42 -0300

    Update comment for Movement class description

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Movement : MonoBehaviour   //"Script de momiviento"
+public class Movement : MonoBehaviour   //"Script de momiviento para juego 3D"
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
