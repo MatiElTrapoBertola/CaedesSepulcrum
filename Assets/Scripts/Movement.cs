f87231a144022e0796fdc39d76dea53c4f3f2258
Matias Bertola <matias_bertola_cabrera@hotmail.com>
2026-09-11 15:59:20 -0300

    Update Movement.cs
    Script de movimiento

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Movement : MonoBehaviour   //"Script de momiviento"
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
